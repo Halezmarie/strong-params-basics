009ab5630b34facea4e038ec3bc4e96e0db8298d
halezmarie <teacherhaleymarie@gmail.com>
2021-06-14 12:48:13 -0500

    commit tree 443c9b36ff04b3ba810c61245826ad95e2343aad

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,14 +27,9 @@ class PostsController < ApplicationController
 		redirect_to post_path(@post)
 	  end
 	  
-	  private
+	private
 	  
-	  
-	  # We pass the permitted fields in as *args;
-	  # this keeps `post_params` pretty dry while
-	  # still allowing slightly different behavior
-	  # depending on the controller action. This
-	  # should come after the other methods
+	
 	  
 	  def post_params(*args)
 		params.require(:post).permit(*args)
